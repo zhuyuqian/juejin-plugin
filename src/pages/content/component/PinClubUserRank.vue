f88af38b11e57adabf1e5d165db2a391ff8c0c82
朱煜乾 <416556102@qq.com>
2023-04-22 03:17:34 +0800

    update：一周沸点样式调整

--- src/pages/content/component/PinClubUserRank.vue
+++ src/pages/content/component/PinClubUserRank.vue
@@ -1,26 +1,26 @@
 <template>
-  <el-card class="pin-club-user-rank" :shadow="false" v-loading="loading">
-    <template #header>
-      <span class="title-box">{{ rankInfo.club.topic.title }}｜本周沸物</span>
-      <span class="desc-box">
+	<el-card class="pin-club-user-rank" :shadow="false" v-loading="loading">
+		<template #header>
+			<span class="title-box">{{ rankInfo.club.topic.title }}｜本周沸物</span>
+			<span class="desc-box">
         最近更新：{{ dayjs(rankInfo.time).format("YYYY-MM-DD HH:mm:ss") }}
       </span>
-    </template>
-    <div class="rank-warp">
-      <div class="rank-box" v-for="(rank,rankIndex) of rankInfo.rank">
-        <div class="rank-title">
-          <span class="title-box">榜{{ rankIndex + 1 }}</span>
-          <span class="count-box">{{ rank.msgCount }}条 / 沸物</span>
-        </div>
-        <div class="user-warp">
-          <div class="user-box" v-for="user of rank.users">
-            <el-avatar shape="square" :size="30" :src="user.userInfo.avatar_large" />
-            <span class="user-name">{{ user.userInfo.user_name }}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </el-card>
+		</template>
+		<div class="rank-warp">
+			<div class="rank-box" v-for="(rank,rankIndex) of rankInfo.rank">
+				<div class="rank-title">
+					<span class="title-box">榜{{ rankIndex + 1 }}</span>
+					<span class="count-box">{{ rank.msgCount }}条 / 沸物</span>
+				</div>
+				<div class="user-warp">
+					<a class="user-box" v-for="user of rank.users" :href="`https://juejin.cn/user/${user.userInfo.user_id}`" target="_blank">
+						<el-avatar shape="square" :size="30" :src="user.userInfo.avatar_large"/>
+						<span class="user-name">{{ user.userInfo.user_name }}</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</el-card>
 </template>
 <script setup>
 import { onMounted, ref } from "vue";
@@ -31,73 +31,79 @@ import { getPinClubHotRank } from "@/pages/background/pins";
 let rankInfo = ref({ time: null, rank: [], club: { topic: {} } });
 let loading = ref(true);
 onMounted(async () => {
-  loading.value = true;
-  let urlInfo = await initUrlInfo();
-  rankInfo.value = await getPinClubHotRank(urlInfo.info.clubId);
-  loading.value = false;
+	loading.value = true;
+	let urlInfo = await initUrlInfo();
+	rankInfo.value = await getPinClubHotRank(urlInfo.info.clubId);
+	loading.value = false;
 });
 </script>
 <style scoped lang="less">
 .pin-club-user-rank {
-  margin-bottom: 10px;
-  margin-top: 10px;
+	margin-bottom: 10px;
+	margin-top: 10px;
 
-  .title-box {
-    font-size: 16px;
-    font-weight: bold;
-    color: #333;
-  }
+	.title-box {
+		font-size: 16px;
+		font-weight: bold;
+		color: #333;
+	}
 
-  .desc-box {
-    font-size: 12px;
-    color: #999;
-  }
+	.desc-box {
+		font-size: 12px;
+		color: #999;
+	}
 
-  .rank-warp {
-    display: flex;
+	.rank-warp {
+		display: flex;
 
-    .rank-box {
-      flex: 1;
+		.rank-box {
+			flex: 1;
 
-      .rank-title {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+			.rank-title {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
 
-        .title-box {
-          font-size: 14px;
-          color: #333;
-        }
+				.title-box {
+					font-size: 14px;
+					color: #333;
+				}
 
-        .count-box {
-          margin-right: 20px;
-          font-size: 12px;
-          color: #999;
-        }
-      }
+				.count-box {
+					margin-right: 20px;
+					font-size: 12px;
+					color: #999;
+				}
+			}
 
-      .user-warp {
-        margin-top: 10px;
-        max-height: 150px;
-        overflow-y: auto;
+			.user-warp {
+				margin-top: 10px;
+				max-height: 150px;
+				overflow-y: auto;
 
-        .user-box {
-          display: flex;
-          align-items: center;
-          margin-bottom: 10px;
+				.user-box {
+					display: flex;
+					align-items: center;
+					margin-bottom: 10px;
 
-          &:last-child {
-            margin-bottom: 0;
-          }
+					&:last-child {
+						margin-bottom: 0;
+					}
 
-          .user-name {
-            margin-left: 15px;
-            font-size: 13px;
-            color: #333;
-          }
-        }
-      }
-    }
-  }
+					.user-name {
+						margin-left: 6px;
+						font-size: 13px;
+						color: #333;
+					}
+
+					&:hover {
+						.user-name {
+							color: #1e80ff;
+						}
+					}
+				}
+			}
+		}
+	}
 }
 </style>
